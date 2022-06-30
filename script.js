fc827815785a362b0db77c925d2d9f88e0274448
Andrej Simeunovic <andrej.simeunovic98@gmail.com>
2022-06-30 13:33:04 +0200

    game in UI done

--- script.js
+++ script.js
@@ -1,99 +1,86 @@
 // TESTING branch
-let playerScore = 0;
-let computerScore = 0;
+let playerScore = document.getElementsByClassName("score")[0];
+let computerScore = document.getElementsByClassName("score")[1];
+
 let playerWins = false;
-let round = 1;
+let round = 0;
 let choices = ["Rock", "Paper", "Scissor"];
+const selection = { rock: "👊", paper: "✋", scissor: "✌️" };
 
-let buttons = document.getElementsByClassName('selections')[0].getElementsByClassName('selection')
-console.log(buttons)
+let buttons = document
+  .getElementsByClassName("selections")[0]
+  .getElementsByClassName("selection");
 
-for(var i = 0; i < buttons.length; i++){
-  var button = buttons[i]
-  button.addEventListener('click',getChoice)
-
+for (var i = 0; i < buttons.length; i++) {
+  var button = buttons[i];
+  button.addEventListener("click", getChoice);
 }
 
-function getChoice(event){
-     console.log(event.target)
-     convertIconToString(event.target.innerText)
+function getChoice(event) {
+  let icon = event.target.innerText;
+  let playerChoice = Object.keys(selection).filter(
+    (e) => selection[e] === icon
+  )[0];
+  let computerChoice = computerPlay();
+  playRound(playerChoice, computerChoice);
 }
-
-function convertIconToString(icon) {
- // TODO
- 
-  
-}
-
 
 function computerPlay() {
-  return choices[Math.floor(Math.random() * choices.length)];
+  return choices[Math.floor(Math.random() * choices.length)].toLowerCase();
 }
 
 function playRound(playerSelection, computerSelection) {
-  console.log("Round: " + round + "\n");
-  round++;
-  let message = "";
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
-  console.log(
-    "players choice: " +
-      playerSelection +
-      "\n" +
-      "computers choice: " +
-      computerSelection
-  );
   switch (playerSelection) {
     case "rock":
       if (computerSelection === "paper") {
-        message = "You Lose! Paper beats Rock";
         playerWins = false;
         updateScore(playerWins);
-        printResult();
+        printSelection(playerSelection, computerSelection);
+        getWinner();
       } else if (computerSelection === "scissor") {
-        message = "You Win! Rock beats Scissor";
         playerWins = true;
         updateScore(playerWins);
-        printResult();
+        printSelection(playerSelection, computerSelection);
+        getWinner();
       } else {
-        message = "It's a tie!";
-        printResult();
+        printSelection(playerSelection, computerSelection);
+        getWinner();
       }
 
       break;
 
     case "scissor":
       if (computerSelection === "rock") {
-        message = "You Lose! Rock beats Scissor";
         playerWins = false;
         updateScore(playerWins);
-        printResult();
+        printSelection(playerSelection, computerSelection);
+        getWinner();
       } else if (computerSelection === "paper") {
-        message = "You Win! Scissor beats  Paper";
         playerWins = true;
         updateScore(playerWins);
-        printResult();
+        printSelection(playerSelection, computerSelection);
+        getWinner();
       } else {
-        message = "It's a tie!";
-        printResult();
+        printSelection(playerSelection, computerSelection);
+        getWinner();
       }
 
       break;
 
     case "paper":
       if (computerSelection === "scissor") {
-        message = "You Lose! Scissor beats Paper";
         playerWins = false;
         updateScore(playerWins);
-        printResult();
+        printSelection(playerSelection, computerSelection);
+        getWinner();
       } else if (computerSelection === "rock") {
-        message = "You Win! Paper beats Rock";
         playerWins = true;
         updateScore(playerWins);
-        printResult();
+        printSelection(playerSelection, computerSelection);
+        getWinner();
       } else {
-        message = "It's a tie!";
-        printResult();
+        printSelection(playerSelection, computerSelection);
+        getWinner();
       }
 
       break;
@@ -101,62 +88,102 @@ function playRound(playerSelection, computerSelection) {
     default:
       break;
   }
-  return message + "\n------------------------------\n";
 }
 
 function updateScore(playerWins) {
   if (playerWins) {
-    playerScore++;
+    playerScore.innerText = parseInt(playerScore.innerText) + 1;
   } else {
-    computerScore++;
+    computerScore.innerText = parseInt(computerScore.innerText) + 1;
   }
 }
 
-function printResult() {
-  console.log("player: " + playerScore + "\ncomputer: " + computerScore);
+function printSelection(player, computer) {
+  let div = document.createElement("div");
+  div.classList.add("round");
+  div.classList.add("display-container");
+  let content = `
+  <span>${selection[player]}</span>
+  <span>${selection[computer]}</span>
+  `;
+  div.innerHTML = content;
+  document.getElementsByClassName("round-container")[0].appendChild(div);
 }
 
-function game() {
-  let playerSelection = prompt("Choose rock, paper or scissor");
+// function printResult() {
+//   console.log("player: " + playerScore + "\ncomputer: " + computerScore);
+// }
 
+// function game() {
+//   let playerSelection = prompt("Choose rock, paper or scissor");
 
-  if (playerSelection !== null) {
-    while (validateInput(playerSelection)) {
-      playerSelection = prompt(
-        "Type Rock, Paper, or Scissors. Spelling needs to be exact, but capitilization doesnt matter"
-      );
-      if (playerSelection === null) {
-        return alert("Thanks for playing!");
-      }
-    }
-    let computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-  } else {
-    return alert("Thanks for playing!");
-  }
+//   if (playerSelection !== null) {
+//     while (validateInput(playerSelection)) {
+//       playerSelection = prompt(
+//         "Type Rock, Paper, or Scissors. Spelling needs to be exact, but capitilization doesnt matter"
+//       );
+//       if (playerSelection === null) {
+//         return alert("Thanks for playing!");
+//       }
+//     }
+//     let computerSelection = computerPlay();
+//     console.log(playRound(playerSelection, computerSelection));
+//   } else {
+//     return alert("Thanks for playing!");
+//   }
 
-  getWinner();
-}
+//   getWinner();
+// }
 
 function getWinner() {
-  if (playerScore > computerScore) {
-    alert("Congratulations! You have won");
-  } else if (playerScore < computerScore) {
-    alert("You have lost to the computer!");
-  } else {
-    alert("It is a draw!");
+  round++;
+  let p = document.createElement("p");
+  if (round === 5) {
+    let playerFinalResult = parseInt(playerScore.innerText);
+    let computerFinalResult = parseInt(computerScore.innerText);
+    if (playerFinalResult > computerFinalResult) {
+      p.innerText = "Congratulations! You have won";
+    } else if (playerFinalResult < computerFinalResult) {
+      p.innerText = "You have lost to the computer!";
+    } else {
+      p.innerText = "It is a draw!";
+    }
+
+    let refresh = document.createElement('p')
+    refresh.innerText = "Please refresh the page to start over"
+
+    document.getElementsByClassName("winner")[0].appendChild(p);
+    document.getElementsByClassName("winner")[0].appendChild(refresh);
+    
+    deleteIcons()
+    
+    
+    // let btn = document.createElement('button')
+    // btn.classList.add('btn')
+    // btn.innerText = "Restart"
+       
+    // document.getElementsByClassName('winner')[0].appendChild(btn);
+
+    //btn.addEventListener('click',restart)
+
   }
-  playerScore = 0;
-  computerScore = 0;
 }
 
-function validateInput(input) {
-  input = input.toLowerCase();
-  let list = choices.map((e) => e.toLowerCase());
-  if (input == false || list.includes(input) == false) {
-    return true;
+
+function deleteIcons() {
+  let parent = document.getElementsByClassName('selections')[0]
+  while (parent.hasChildNodes()) {
+    parent.removeChild(parent.firstChild)
+    
   }
+
 }
+
+// function validateInput(input) {
+//   input = input.toLowerCase();
+//   let list = choices.map((e) => e.toLowerCase());
+//   if (input == false || list.includes(input) == false) {
+//     return true;
+//   }
+// }
 //game();
-
-
